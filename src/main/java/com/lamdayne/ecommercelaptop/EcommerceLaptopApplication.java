9e9953b069bad6074f2c7136ed37adf8adcee43f
lamdayne <nlam32428@gmail.com>
2026-01-13 14:36:02 +0700

    init project

---
+++ src/main/java/com/lamdayne/ecommercelaptop/EcommerceLaptopApplication.java
@@ -0,0 +1,13 @@
+package com.lamdayne.ecommercelaptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceLaptopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceLaptopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lamdayne/ecommercelaptop/EcommerceLaptopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamdayne.ecommercelaptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceLaptopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
