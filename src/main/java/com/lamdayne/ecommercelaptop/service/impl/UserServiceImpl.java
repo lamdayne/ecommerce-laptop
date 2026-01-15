f031348495e6502880c33b4bf0f3d51a2b562135
lamdayne <nlam32428@gmail.com>
2026-01-15 15:36:59 +0700

    feat(service): add UserService and implements UserService

---
+++ src/main/java/com/lamdayne/ecommercelaptop/service/impl/UserServiceImpl.java
@@ -0,0 +1,67 @@
+package com.lamdayne.ecommercelaptop.service.impl;
+
+import com.lamdayne.ecommercelaptop.constant.Role;
+import com.lamdayne.ecommercelaptop.dto.request.CreateUserRequest;
+import com.lamdayne.ecommercelaptop.dto.request.UpdateUserRequest;
+import com.lamdayne.ecommercelaptop.dto.response.UserResponse;
+import com.lamdayne.ecommercelaptop.entity.User;
+import com.lamdayne.ecommercelaptop.exception.AppException;
+import com.lamdayne.ecommercelaptop.exception.ErrorCode;
+import com.lamdayne.ecommercelaptop.mapper.UserMapper;
+import com.lamdayne.ecommercelaptop.repository.UserRepository;
+import com.lamdayne.ecommercelaptop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserResponse getUserById(String userId) {
+        return userMapper.toUserResponse(userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND)));
+    }
+
+    @Override
+    public UserResponse createUser(CreateUserRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new AppException(ErrorCode.EMAIL_EXISTS);
+        } else if (userRepository.existsByPhone(request.getPhone())) {
+            throw new AppException(ErrorCode.PHONE_EXISTS);
+        }
+
+        User user = userMapper.toUser(request);
+        user.setRole(Role.USER);
+
+        try {
+            user = userRepository.save(user);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        return userMapper.toUserResponse(user);
+    }
+
+    @Override
+    public UserResponse updateUser(String userId, UpdateUserRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        userMapper.updateUser(user, request);
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+}
