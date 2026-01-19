515db63a17ab53789a590d26d2c35561a2bc1d9b
lamdayne <nlam32428@gmail.com>
2026-01-19 21:55:49 +0700

    feat(service): add feature upload image use cloudinary

---
+++ src/main/java/com/lamdayne/ecommercelaptop/service/UploadImageFileService.java
@@ -0,0 +1,9 @@
+package com.lamdayne.ecommercelaptop.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface UploadImageFileService {
+    String uploadImageFile(MultipartFile file) throws IOException;
+}
