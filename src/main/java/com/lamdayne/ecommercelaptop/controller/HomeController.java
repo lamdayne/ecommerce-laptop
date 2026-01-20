e75a9435df28dec76a19c8aebe877d1332a370a6
khoa-k2 <khoavu.vndk@gmail.com>
2026-01-20 15:34:07 +0700

    update

--- src/main/java/com/lamdayne/ecommercelaptop/controller/HomeController.java
+++ src/main/java/com/lamdayne/ecommercelaptop/controller/HomeController.java
@@ -2,18 +2,13 @@ package com.lamdayne.ecommercelaptop.controller;
 
 
 import com.lamdayne.ecommercelaptop.dto.response.ProductResponse;
-import com.lamdayne.ecommercelaptop.entity.Product;
-import com.lamdayne.ecommercelaptop.repository.CategoryRepository;
-import com.lamdayne.ecommercelaptop.repository.ProductRepository;
 import com.lamdayne.ecommercelaptop.service.CategoryService;
 import com.lamdayne.ecommercelaptop.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 @RequiredArgsConstructor
--- src/main/java/com/lamdayne/ecommercelaptop/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lamdayne.ecommercelaptop.repository;
-
-import com.lamdayne.ecommercelaptop.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
