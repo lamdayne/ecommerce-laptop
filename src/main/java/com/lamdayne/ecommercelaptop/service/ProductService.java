ef0ff1eb146b090a1e0a794ad29195385ca19e20
lamdayne <nlam32428@gmail.com>
2026-01-20 13:42:48 +0700

    feat(product): add full CRUD for Products

---
+++ src/main/java/com/lamdayne/ecommercelaptop/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.lamdayne.ecommercelaptop.service;
+
+import com.lamdayne.ecommercelaptop.dto.request.CreateProductRequest;
+import com.lamdayne.ecommercelaptop.dto.request.UpdateProductRequest;
+import com.lamdayne.ecommercelaptop.dto.response.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface ProductService {
+    ProductResponse createProduct(CreateProductRequest request);
+    ProductResponse updateProduct(String id, UpdateProductRequest request);
+    void deleteProduct(String id);
+    ProductResponse getProductById(String id);
+    List<ProductResponse> getAllProducts();
+    ProductResponse uploadImageProduct(String id, MultipartFile file) throws IOException;
+}
